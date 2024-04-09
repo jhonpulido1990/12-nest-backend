0c2ee5b04ec3e1005417845d5872dc06bfd68417
jhon-puli <3825@holbertonstudents.com>
2024-04-09 09:55:54 -0500

    variable de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
 
 @Module({
+  // eslint-disable-next-line prettier/prettier
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
   ],
   controllers: [],
